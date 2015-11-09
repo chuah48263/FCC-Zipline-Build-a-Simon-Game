1b13117676505d03914766cf94560ab3fc5cab29
chuah48263 <chuah48263@gmail.com>
2015-11-09 08:25:48 +0800

    complete ui

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,40 @@
 'use strict';
 
-app.controller('MainController', ['$scope', function($scope) {}]);
+app.controller('MainController', ['$scope', '$timeout', function($scope,
+  $timeout) {
+  $scope.count = '&nbsp;&nbsp;';
+
+  $scope.onOff = false;
+  $scope.switch = function() {
+    if (!$scope.onOff) {
+      $scope.onOff = true;
+      $scope.count = '--';
+    } else if ($scope.onOff) {
+      $scope.onOff = false;
+      $scope.started = false;
+      $scope.stricted = false;
+      $scope.count = '&nbsp;&nbsp;';
+    }
+  };
+
+  $scope.flash = false;
+  $scope.started = false;
+  $scope.start = function() {
+    if ($scope.onOff) {
+      $scope.started = true;
+      $scope.flash = true;
+      $timeout(function(){
+        $scope.flash = false;
+      }, 1000);
+    }
+  };
+
+  $scope.stricted = false;
+  $scope.strict = function() {
+    if ($scope.onOff && !$scope.stricted) {
+      $scope.stricted = true;
+    } else if ($scope.onOff && $scope.stricted) {
+      $scope.stricted = false;
+    }
+  };
+}]);
